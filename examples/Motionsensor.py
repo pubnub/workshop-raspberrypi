571f15138a2915f2f48b823589698337fb07ac16
Bhavana Srinivas <bana.srini@gmail.com>
2015-03-30 14:16:58 -0700

    Update Motionsensor.py

--- examples/Motionsensor.py
+++ examples/Motionsensor.py
@@ -16,7 +16,7 @@ ssl_on = len(sys.argv) > 5 and bool(sys.argv[5]) or False
 ## -----------------------------------------------------------------------
 pubnub = Pubnub(publish_key=publish_key, subscribe_key=subscribe_key, secret_key=secret_key, cipher_key=cipher_key, ssl_on=ssl_on)
 channel = 'motionsensor'
-message = 'Motion Detected'
+message = {columns: [['data', 1]]}
 
 
 # Asynchronous usage
