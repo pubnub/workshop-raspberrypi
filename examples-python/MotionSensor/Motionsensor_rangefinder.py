2bf2ee9252a8db803a8afb3281e120803f694a64
Bhavana Srinivas <bhavana@pubnubs-MacBook-Air-94.local>
2015-04-01 10:23:10 -0700

    edits

--- examples-python/MotionSensor/Motionsensor_rangefinder.py
+++ examples-python/MotionSensor/Motionsensor_rangefinder.py
@@ -15,7 +15,7 @@ ssl_on = len(sys.argv) > 5 and bool(sys.argv[5]) or False
 ## Initiate Pubnub State
 ## -----------------------------------------------------------------------
 pubnub = Pubnub(publish_key=publish_key, subscribe_key=subscribe_key, secret_key=secret_key, cipher_key=cipher_key, ssl_on=ssl_on)
-channel = 'motionrange'
+channel = 'Rangefinder'
 message = {'motion' : 1}
 
 
@@ -23,11 +23,12 @@ message = {'motion' : 1}
 def callback(message):
   print(message)
 
- 
+# Setting the pin number as a variable so its easier to use, and setting it to receive an input.
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
-  
+
+# calling publish, which sends a message over the channel onto the internet
 def MOTION(PIR_PIN):
   pubnub.publish(channel, message, callback=callback, error=callback)
 
@@ -35,6 +36,7 @@ print “PIR Module Test (CTRL+C to exit)”
 time.sleep(2)
 print “Ready”
 
+# calling the publish only when the sensor detects motion
 try:
   GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
   while 1:
