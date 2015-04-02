d1e7971f0e45de247973455fadfef6a33d4df554
Ian Jennings <ian@meetjennings.com>
2015-04-01 22:59:39 -0500

    motion demo

--- examples-nodejs/eon/motion.js
+++ examples-nodejs/eon/motion.js
@@ -1,1 +1,17 @@
-motion.js
+var PUBNUB = require('pubnub');
+
+var pubnub = PUBNUB.init({
+  publish_key: 'demo',
+  subscribe_key: 'demo'
+});
+
+setInterval(function(){
+
+  pubnub.publish({
+    channel: "c3-gauge-rasp-demo",
+    message: {
+      columns: [['data', Math.random() * 99]]
+    }
+  });
+
+}, 1000);
