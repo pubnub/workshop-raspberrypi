5e3c37f0e3525e28471afc8901f659a6a7f7f3b9
Tomomi ❤ Imura <tomomi.i@gmail.com>
2016-02-11 08:45:59 -0800

    Update hello.js

--- projects-nodejs/hello-world/hello.js
+++ projects-nodejs/hello-world/hello.js
@@ -1,9 +1,9 @@
 var pubnub = require('pubnub').init({
-	publish_key : 'demo',
-	subscribe_key : 'demo'
+	publish_key : 'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695',
+	subscribe_key : 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe'
 });
 
-var channel = 'hello';
+var channel = 'hello-pi';
 
 var username = 'Your name';
 var message = 'Hello World from Pi!';
